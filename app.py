665ff2873f9bef4a9847dacec17a5f6bc70ee2f1
hqb199620010-hash <hqb199620010@gmail.com>
2026-05-11 16:00:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -196,9 +196,9 @@ if not df.empty:
     c11.metric("HCl", f"{latest['# HCl']} mg/Nm3")
     c12.metric("NO", f"{latest['# N0']} mg/Nm3")
 
- st.markdown("---")
- with st.expander("📝 Nhật ký dữ liệu"):
+st.markdown("---")
+with st.expander("📝 Nhật ký dữ liệu"):
         st.dataframe(df.tail(10), use_container_width=True)
 else:
-    st.warning("Đang kết nối dữ liệu...")
+st.warning("Đang kết nối dữ liệu...")
 
