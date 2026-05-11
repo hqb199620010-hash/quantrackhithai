1809fc57d90082f4e8402fca7f453e900fc24e66
hqb199620010-hash <hqb199620010@gmail.com>
2026-05-11 15:51:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ if not df.empty:
     c9.metric("NO2", f"{latest['# N02 (mg/Nm3)']} mg/Nm3")
     c10.metric("NOx", f"{latest['# N0X (mg/Nm3)']} mg/Nm3")
     c11.metric("HCl", f"{latest['# HCl']} mg/Nm3")
-    c12.metric("NO", f"{latest['# N0']} mg/Nm3"
+    c12.metric("NO", f"{latest['# N0']} mg/Nm3")
 
 st.markdown("---")
     with st.expander("📝 Nhật ký dữ liệu"):
